769f0b26101d717a5c0b8abd0937655f546ca6a1
jamalchelski <ifista201522035@gmail.com>
2020-02-20 16:50:57 +0700

    adding configuration for circleCi

---
+++ src/components/atoms/IconBank/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const IconBank = () => {
+    return(
+        <View>
+        <Text>Hello this is new code</Text>
+    </View>
+    )
+}
+export default IconBank;
